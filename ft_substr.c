775a144b6ba5dd7f097323f67e60a3dad7142ef9
Daniela Maria Da Silva Ornelas <danielad@c2r2s4.42lausanne.ch>
2025-10-21 15:58:18 +0200

    changed 1 condition in substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,11 @@
 /*   By: danielad <danielad@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 15:52:38 by danielad          #+#    #+#             */
-/*   Updated: 2025/10/21 10:25:27 by danielad         ###   ########.fr       */
+/*   Updated: 2025/10/21 15:41:34 by danielad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
-#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -24,10 +22,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	len_of_s = (unsigned int)ft_strlen(s);
-	if (start > len_of_s)
+	if (start >= len_of_s || len_of_s == 0)
 		return (ft_strdup(""));
 	if (len > (len_of_s + start))
-		len = (len_of_s + start);
+		len = len_of_s + start;
 	substr = ft_calloc(len + 1, sizeof(char));
 	if (substr == NULL)
 		return (NULL);
@@ -40,6 +38,30 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (substr);
 }
 
+// char	*ft_substr(const char *s, unsigned int start, size_t len)
+// {
+// 	size_t			i;
+// 	size_t			j;
+// 	unsigned int	lens;
+// 	char			*res;
+
+// 	i = start;
+// 	j = 0;
+// 	if (!s)
+// 		return (NULL);
+// 	lens = ft_strlen(s);
+// 	if (start >= lens)
+// 		return (ft_strdup(""));
+// 	if (lens - start < len)
+// 		len = lens - start;
+// 	res = (char *)malloc(sizeof(char) * len + 1);
+// 	if (!res)
+// 		return (NULL);
+// 	while (j < len)
+// 		res[j++] = s[i++];
+// 	return (res);
+// 	res[j] = '\0';
+// }
 //     ft_substr -- extract a substring from a string
 // DESCRIPTION
 //     Allocate (with malloc(3)) and return a new string from the string s.
